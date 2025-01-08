343585dff049c671dc2f84ee92d5b73e99ea55c2
Shivabhandari122 <qshiva906@gmail.com>
2025-01-08 21:15:16 +0545

    creating author table in database

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+require('dotenv').config()
 const express = require('express')
-const connectToDatabase = require('./database')
 const app = express()
+const connectToDatabase = require('./database')
+app.use(express.json())
 
 connectToDatabase()
 
+app.listen(process.env.PORT, () =>{
+    console.log("project has been started")
+})
+
+app.post('/authors', (req, res) =>{
+    console.log(req.body)
+    res.status(201).json({
+        message: "my Api run"
+    })
+})
--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connectToDatabase(){
-    mongoose.connect('mongodb+srv://qshiva906:whitehouse@cluster0.6ftcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect(process.env.MONGODB_URI)
     console.log('Database connected sucessfully')
 }
 
