7ac6baf98f80b9f2904f0769aacf9a1e43badd4a
Shivabhandari122 <qshiva906@gmail.com>
2025-01-17 11:28:16 +0545

    update

--- model/usersModel.js
+++ model/usersModel.js
@@ -1,20 +1,30 @@
 const mongoose = require('mongoose')
-const schema = mongoose.Schema
+const bcrypt = require('bcrypt');
 
-const usersSchema = new schema({
+const userSchema =  new mongoose.Schema({
     username: {
-        type : String,
-        required: true
-    },
-    email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        required: true
     }
 })
 
-const Users = mongoose.model('users', usersSchema)
-module.exports = Users
+userSchema.pre('save', async function( next) {
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+}
+)
+
+const User =  mongoose.model('User', userSchema);
+
+module.exports = User;
